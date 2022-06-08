02c5d6072d3b2165de2308c6d3b789e898114f82
kjerybar <105828465+kjerybar@users.noreply.github.com>
2022-06-08 15:53:40 -0700

    Update pyTTi-TF930.py

--- pyTTi-TF930.py
+++ pyTTi-TF930.py
@@ -2,7 +2,7 @@ import serial
 import time
 import csv
 
-FILENAME = time.strftime("%Y-%m-%d-%H%M.%Ss")
+
 
 def get_freq(collection_period : int):
     """Gets the frequency and calls other helper function for plotting or logging the data
@@ -16,29 +16,26 @@ def get_freq(collection_period : int):
         int: acquired data points
     """
     try:
+        filename = time.strftime("%Y-%m-%d-%H%M.%Ss")
         cur_time = time.time() # Takes the time
         collection_time = cur_time + collection_period
         starting_time = cur_time
 
-        # data = []
-        # time_collected = []
-        # df_list = []
-        
         # Initialize output file
         header = ["Frequency(Hz)","Time(s)"]
-        with open(FILENAME+".csv", 'w', newline = '') as csvfile:
+        
+        # Initialize serial port with device defaults from device manual
+        s = serial.Serial("COM1" ,baudrate = 115200, stopbits = serial.STOPBITS_ONE , timeout = 1,parity=serial.PARITY_NONE, rtscts=True, dsrdtr = True)
+        s.flush()
+
+        # Initialize serial stream
+        # send command to start receiving data
+        s.write(b'E?\n\r')
+
+        # Loop reading stream until time expires
+        with open(filename+".csv", 'w', newline = '') as csvfile:
             f_writer = csv.DictWriter(csvfile, fieldnames = header)
             f_writer.writerow({"Frequency(Hz)":"Frequency(Hz)","Time(s)":"Time(s)"})
-
-            # Initialize serial port with device defaults from device manual
-            s = serial.Serial("COM1" ,baudrate = 115200, stopbits = serial.STOPBITS_ONE , timeout = 1,parity=serial.PARITY_NONE, rtscts=True, dsrdtr = True)
-            s.flush()
-
-            # Initialize serial stream
-            # send command to start receiving data
-            s.write(b'E?\n\r')
-
-            # Loop reading stream until time expires
             while cur_time <= collection_time:
                 # Reads the data off the counter
                 byte_data = s.readline()                
@@ -53,31 +50,18 @@ def get_freq(collection_period : int):
 
                 # Write freq_data_fl and time_col to output file 
                 time_col = cur_time - starting_time
-                # data.append(freq_data_fl)
-                # time_collected.append(time_col)
+                
                 #Write to file
                 f_writer.writerow({"Frequency(Hz)":freq_data_fl,"Time(s)":time_col})
 
-    
-                # Get next reading
-                s.write(b'N?\n\r')
                 cur_time = time.time()
                 
-
-            # df_list.append(data)
-            # df_list.append(time_collected)
-            # dbf = pde.DataFrame(df_list, index = ["Frequency(Hz)","Time(s)"]).T
-            # dbf.to_csv("{}.csv".format(FILENAME), index = False , header = True)
-            s.write(b'STOP\n\r') 
-            print("done with no error")
+        s.write(b'STOP\n\r') 
+        print("done with no error")
 
     except serial.SerialException as e:
-            #df_list.append(data)
-            #df_list.append(time_collected)
-            #dbf = pde.DataFrame(df_list, index = ["Frequency(Hz)","Time(s)"]).T
-            #dbf.to_csv("{}.csv".format(FILENAME), index = False , header = True)
-            s.write(b'STOP\n\r')
-            print("raised Serial error didn't take all data"+ e)
+        s.write(b'STOP\n\r')
+        print("raised Serial error didn't take all data"+ e)
     except IndexError:
         s.write(b'STOP\n\r')
         print("Switch to B")
